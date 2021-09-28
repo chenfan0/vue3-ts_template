7642c90b2cf68a3982dd3012e2d453e1788e45ba
陈帆 <8608132+jiuzhejiuzhe@user.noreply.gitee.com>
2021-09-28 19:17:00 +0800

    feat: 修改一些小问题

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,11 +6,6 @@ const routes: Array<RouteRecordRaw> = [
     path: '/',
     name: 'Home',
     component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ]
 
